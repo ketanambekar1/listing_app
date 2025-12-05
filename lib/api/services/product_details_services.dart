006c3253ab20f3738de8b5ed407be7235bc286fd
ketanambekar <krdambekar@gmail.com>
2025-12-05 10:48:13 +0530

    null handling

--- lib/api/services/product_details_services.dart
+++ lib/api/services/product_details_services.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:listing_app/api/api_constants.dart';
 import 'package:listing_app/api/dio_client.dart';
-import 'package:listing_app/constants/app_constants.dart';
 import 'package:listing_app/data/models/product_details_model.dart';
 
 class ProductDetailsServices {
@@ -10,9 +11,30 @@ class ProductDetailsServices {
         "${ApiConstants.productsDetailsById}$id",
       );
 
+      // Validate Directus-style response
+      if (response.data == null || response.data["data"] == null) {
+        Get.snackbar(
+          "Error",
+          "No product data found.",
+          snackPosition: SnackPosition.BOTTOM,
+        );
+        return null;
+      }
+
       return ProductDetailsModel.fromJson(response.data["data"]);
     } catch (e) {
       print("ProductService Error: $e");
+      Get.back();
+      Get.snackbar(
+        "Oops!",
+        "Unable to load product details.",
+        snackPosition: SnackPosition.BOTTOM,
+        margin: const EdgeInsets.all(16),
+        borderRadius: 12,
+        backgroundColor: const Color(0xFFff4444),
+        colorText: const Color(0xFFFFFFFF),
+      );
+
       return null;
     }
   }
